566d46312e2fa91569a4dae6b0ae974bf8a646b2
Carl Middleton <dev@carlCodes.com>
2018-03-18 12:54:53 -0700

    Feature complete, working, ready to optimize

--- host.py
+++ host.py
@@ -1,11 +1,16 @@
+import json
 from flask import Flask
 from flask import request
+from flask import Response
 from chopper import Chopper
 from storeybook import Storey
+from xml.etree import ElementTree
 
 app = Flask(__name__)
 
 BASE_URL = 'http://localhost'
+PORT = 4000
+ENCODER = 'json'  # 'json' or 'xml', json only at the moment
 
 
 @app.route("/")
@@ -27,9 +32,9 @@ def post_pdf():
     print('Made doc')
     if storey.contains_prefix(doc.get_file_key()):
         print('Key conflict')
-        return 'Error!'
+        return build_response('Error!  Document already exists.', 400)
     print('Saving')
-    return save(doc)
+    return build_response(save(doc), 200)
 
 
 @app.route("/docrepo", methods=['PUT'])
@@ -48,21 +53,21 @@ def put_pdf():
     if len(conflicts) > 0:
         success = storey.delete_many(conflicts)
         if not success:
-            #todo - should we perhaps silently let this go 'cause concurrency?
+            # todo - should we perhaps silently let this go 'cause concurrency?
             print('Error!  Could not delete duplicates.')
-            return 'Error!  Could not delete duplicates.'
+            return build_response('Error!  Could not delete duplicates.', 400)
 
-    return save(doc)
+    return build_response(save(doc), 200)
 
 
 @app.route("/docrepo/<image>", methods=['GET'])
-def get_pdf(image):
+def get_pdf_page(image):
     print(image)
 
     if not storey.contains_prefix(image):
-        return 'ERROR! No key! Looked for: ' + image
+        return build_response('ERROR! No key! Looked for: ' + image, 404)
 
-    return storey.get(image)
+    return build_response(storey.get(image), 200)
 
 
 def save(chop):
@@ -77,11 +82,29 @@ def save(chop):
         print('Chopped one.')
         storey.save(page_key, image)
         print('Saved')
-        page_urls.append(BASE_URL + '/' + page_key)
+        page_urls.append(BASE_URL + ':' + str(PORT) + '/docrepo/' + page_key)
     print('Final list:' + str(page_urls))
-    return page_urls
+
+    return {num+1: value for num, value in enumerate(page_urls)}
+
+
+def build_response(input_obj, status):
+    encoded = None
+    encoder = None
+    print(type(input_obj))
+
+    if type(input_obj) is bytes:
+        encoded = input_obj
+        encoder = 'image/png'
+    elif ENCODER == 'json':
+        encoded = json.dumps(input_obj)
+        encoder = 'application/json'
+    elif ENCODER == 'xml':
+        # todo - xml output
+        pass
+    return Response(encoded, status=status, mimetype=encoder)
 
 
 storey = Storey()
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
